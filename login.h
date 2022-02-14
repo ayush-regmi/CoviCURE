6f62d833b4f97c19e21bd6035edc38e20d19caf6
PrabeshTimilsina <98795137+PrabeshTimilsina@users.noreply.github.com>
2022-02-14 19:48:06 +0545

    Created a Landing page and login page.
    I have added pictures which might not work properly. And change the address of the pictures when you are working on it creating a branch.

---
+++ login.h
@@ -0,0 +1,25 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = nullptr);
+    ~Login();
+
+private slots:
+    void on_pushButton_login_clicked();
+
+private:
+    Ui::Login *ui;
+};
+
+#endif // LOGIN_H
