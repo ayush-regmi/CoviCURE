bd0fdda70cf6784e03a610bb167d46d412f4c1bd
PrabeshTimilsina <98795137+PrabeshTimilsina@users.noreply.github.com>
2022-02-26 10:34:49 +0545

    first 3 pages of our app
    for dashboard go to login.cpp and there is an if condition where if the password and email are right we have to make the mainwindow of dashboard appear

---
+++ signup.cpp
@@ -0,0 +1,21 @@
+#include "signup.h"
+#include "ui_signup.h"
+
+Signup::Signup(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Signup)
+{
+    ui->setupUi(this);
+
+//    QPixmap pix("F:/2nd semester/COMP/Project/covid.jpg");
+//        int w = ui->label_pic->width();
+//        int h = ui->label_pic->height();
+//        ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+
+
+}
+
+Signup::~Signup()
+{
+    delete ui;
+}
