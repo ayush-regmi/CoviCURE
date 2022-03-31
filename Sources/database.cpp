170e9a5cec4ebbf46d3d74e26a3a3100943040fa
AjushRG <ayushregmi736@gmail.com>
2022-03-31 18:48:37 +0545

    added some database

--- Sources/database.cpp
+++ Sources/database.cpp
@@ -20,42 +20,22 @@
          db.open();
          if(!db.open())
          {
-            qDebug()<<" Can't connect "<<"connect to mysql error"<<db.lastError().text();
+           // qDebug()<<" Can't connect "<<"connect to mysql error"<<db.lastError().text();
              return false;
          }
          else
          {
-           qDebug()<<" Successful connection "<<"connect to mysql OK";
+          // qDebug()<<" Successful connection "<<"connect to mysql OK";
               return true;
          }
 
-         QSqlQuery query(db);
-             // This is the name of my watch , The next operation is to print out test surface name All the data in a column
-             query.exec("select * from test");
-             while(query.next()){
-                 qDebug()<<query.value("name").toString();
-             }
+//         QSqlQuery query(db);
+//             // This is the name of my watch , The next operation is to print out test surface name All the data in a column
+//             query.exec("select * from test");
+//             while(query.next()){
+//                 qDebug()<<query.value("name").toString();
+//             }
       }
 
 
-//        db = QSqlDatabase::addDatabase("QMYSQL"); // Initializing Database, QMYSQL is MySQL database driver for QT.
-//        db.setHostName("127.0.0.1"); // Setting hostname
-//        db.setPort(3306); // setting port
-//        db.setDatabaseName("sql_store"); // setting database name
-//        db.setUserName("root"); //setting username
-//        db.setPassword("th@nky0u@lm!ghty"); //setting password
-//        qDebug()<<" Start connecting ";
-//        if(db.open()) {
-//            qDebug()<<" Can't connect "<<"connect to mysql error"<<db.lastError().text();
-//            return true;
-//        }
-//        else { // If database connection is not established
-//            qDebug()<<" Successful connection "<<"connect to mysql OK";
-//            return false;
-//        }
- //   }
-
- //  QSqlDatabase Database::getDB(){
-    //   return db;
- //  }
 
--- Sources/login.cpp
+++ Sources/login.cpp
@@ -17,7 +17,7 @@ Login::Login(QWidget *parent) :
 
    QPixmap pix(":/resources/img/doctor.png");
 
-       ui->label_3->setPixmap(pix.scaled(150,150,Qt::KeepAspectRatio));
+       ui->picture->setPixmap(pix.scaled(150,150,Qt::KeepAspectRatio));
 
 }
 
@@ -31,13 +31,66 @@ void Login::on_pushButton_login_clicked()
     QString email=ui->lineEdit_email->text();
     QString password=ui->lineEdit_password->text();
 
-    if(email=="ayush@gmail.com" && password=="ayush")
-    {
-        dashboard = new Dashboard(this);
-        dashboard -> showMaximized();
-    }
-    else{
-         QMessageBox::warning(this,"Login", "Username or password is not correct");
-    }
-}
 
+    //Initial dummy code for login
+//    if(email=="ayush@gmail.com" && password=="ayush")
+//    {
+//        dashboard = new Dashboard(this);
+//        dashboard -> show();
+//    }
+//    else{
+//         QMessageBox::warning(this,"Login", "Username or password is not correct");
+//    }
+
+//    if(email=="ayush@gmail.com" && password=="ayush")
+//    {
+//        dashboard = new Dashboard(this);
+//        dashboard -> showMaximized();
+//    }
+//    else{
+//         QMessageBox::warning(this,"Login", "Username or password is not correct");
+//    }
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL", "MyConnect");
+        db.setHostName("covicure.mysql.database.azure.com");      // Connect to the database host name , Here we need to pay attention to （ If it is ”127.0.0.1“, Unable to connect , Is changed to localhost)
+        db.setPort(3306);                 // Connection database port number , Consistent with settings
+         db.setDatabaseName("user");      // Connection database name , Consistent with settings
+         db.setUserName("Pitchers@covicure");          // Database user name , Consistent with settings
+         db.setPassword("C0v!Cure");    // Database password , Consistent with settings
+         qDebug()<<" Start connecting ";
+
+            if (db.open()){
+                  qDebug()<<" sql connection successful ";
+                  QSqlQuery query(QSqlDatabase::database("MyConnect"));
+
+                  query.prepare(QString("SELECT * FROM users_registration_table WHERE Email=:Email AND Password= :Password"));
+                  query.bindValue(":Email", email);
+                  query.bindValue(":Password", password);
+
+                  if(!query.exec()){
+                      qDebug()<<" Can't connect###"<<"connect to mysql error"<<db.lastError().text();
+                  }
+                  else{
+                       qDebug()<<" Successful connection "<<"connect to mysql OK";
+                      while(query.next()){
+                          QString emaildb=query.value(3).toString();
+                          QString passworddb=query.value(5).toString();
+
+                          if(emaildb==email && passworddb==password){
+                                qDebug()<<"Login Successful !!!";
+                                dashboard = new Dashboard(this);
+                                dashboard -> show();
+                          }
+                          else{
+                               QMessageBox::warning(this,"Login", "Username or password is not correct");
+                          }
+
+
+                      }
+                  }
+            }
+
+            else {
+                 qDebug()<<" Can't connect "<<"connect to mysql error"<<db.lastError().text();
+            }
+
+}
